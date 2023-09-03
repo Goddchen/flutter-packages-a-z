b13abae9b9d8687456fca81e24865af486bf982b
Goddchen <goddchen@gmail.com>
2023-09-03 20:09:34 +0200

    test: use TitleSlide in dummy test

--- test/dummy_test.dart
+++ test/dummy_test.dart
@@ -1,5 +1,9 @@
+import 'package:flutter_packages_a_z/slides/title/title_slide.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Dummy test, remove once real tests are added', () {});
+  testWidgets('Dummy test, remove once real tests are added',
+      (WidgetTester widgetTester) async {
+    await widgetTester.pumpWidget(const TitleSlide());
+  });
 }
