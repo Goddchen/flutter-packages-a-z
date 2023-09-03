a452f880c9382a9b50d1629fb154f4906e115e91
Goddchen <goddchen@gmail.com>
2023-09-03 20:13:10 +0200

    test: fix test

--- test/dummy_test.dart
+++ test/dummy_test.dart
@@ -1,9 +1,16 @@
+import 'package:flutter_deck/flutter_deck.dart';
 import 'package:flutter_packages_a_z/slides/title/title_slide.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Dummy test, remove once real tests are added',
       (WidgetTester widgetTester) async {
-    await widgetTester.pumpWidget(const TitleSlide());
+    await widgetTester.pumpWidget(
+      FlutterDeckApp(
+        slides: const [
+          TitleSlide(),
+        ],
+      ),
+    );
   });
 }
