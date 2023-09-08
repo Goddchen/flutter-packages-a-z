705df15e13a9b59f1a3f5ef30edaa0aa4de3dc3c
Goddchen <goddchen@gmail.com>
2023-09-08 23:06:46 +0200

    feat: add just_audio (#9)

--- lib/slides/just_audio/just_audio_slide.dart
+++ lib/slides/just_audio/just_audio_slide.dart
@@ -4,30 +4,29 @@ import 'package:flutter_packages_a_z/constants.dart';
 import 'package:flutter_packages_a_z/widgets/bullet_point.dart';
 import 'package:flutter_packages_a_z/widgets/package_badge.dart';
 
-class BeamerSlide extends FlutterDeckSplitSlide {
-  const BeamerSlide({super.key})
+class JustAudioSlide extends FlutterDeckSplitSlide {
+  const JustAudioSlide({super.key})
       : super(
           configuration: const FlutterDeckSlideConfiguration(
-            header: FlutterDeckHeaderConfiguration(title: 'beamer'),
-            route: '/beamer',
-            steps: 4,
+            header: FlutterDeckHeaderConfiguration(title: 'just_audio'),
+            route: '/just-audio',
+            steps: 6,
           ),
         );
 
   @override
   Widget left(BuildContext context) => Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           const PackageBadge(
-            author: 'beamer.dev',
-            likes: 1113,
-            name: 'beamer',
-            version: '1.5.6',
+            author: 'ryanheise.com',
+            likes: 2954,
+            name: 'just_audio',
+            version: '0.9.34',
           ),
           const SizedBox(height: 32),
           Text(
-            '''Beamer uses the power of Router and implements all the underlying logic for you, letting you explore arbitrarily complex navigation scenarios with ease.''',
+            '''just_audio is a feature-rich audio player for Android, iOS, macOS, web, Linux and Windows.''',
             style: Theme.of(context).textTheme.headlineMedium,
           ),
         ],
@@ -35,59 +34,55 @@ class BeamerSlide extends FlutterDeckSplitSlide {
 
   @override
   Widget right(BuildContext context) => FlutterDeckSlideStepsBuilder(
-        builder: (BuildContext context, int stepNumber) => Column(
+        builder: (_, int stepNumber) => Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             AnimatedOpacity(
               opacity: stepNumber >= 2 ? 1 : 0,
               duration: stepAnimationDuration,
-              child: const BulletPoint(text: 'Built on top of Navigator 2'),
+              child: const BulletPoint(text: 'Easy to use for basic use cases'),
             ),
             AnimatedOpacity(
               opacity: stepNumber >= 3 ? 1 : 0,
               duration: stepAnimationDuration,
               child: const BulletPoint(
                 text:
-                    '''Works with BeamLocations, which contain a stack of BeamPages''',
+                    '''Play, pause, stop, seek, volume, speed, playlists, clips, ...''',
               ),
+            ),
+            AnimatedOpacity(
+              opacity: stepNumber >= 4 ? 1 : 0,
+              duration: stepAnimationDuration,
+              child: const BulletPoint(text: 'Multiple parallel players'),
+            ),
+            AnimatedOpacity(
+              opacity: stepNumber >= 5 ? 1 : 0,
+              duration: stepAnimationDuration,
+              child: const BulletPoint(text: 'Streaming (DASH, HLS)'),
             ),
             const SizedBox(height: 32),
             AnimatedOpacity(
-              opacity: stepNumber >= 4 ? 1 : 0,
+              opacity: stepNumber >= 6 ? 1 : 0,
               duration: stepAnimationDuration,
               child: const Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   FlutterDeckCodeHighlight(
-                    code: r'$ flutter pub add beamer',
+                    code: r'$ flutter pub add just_audio',
                     language: 'bash',
                     textStyle: TextStyle(fontSize: 24),
                   ),
                   SizedBox(height: 16),
                   FlutterDeckCodeHighlight(
                     code: '''
-final routerDelegate = BeamerDelegate(
-  locationBuilder: RoutesLocationBuilder(
-    routes: {
-      '/': (context, state, data) => HomeScreen(),
-      // ...
-    },
-  ),
-);''',
-                    textStyle: TextStyle(fontSize: 24),
-                  ),
-                  SizedBox(height: 16),
-                  FlutterDeckCodeHighlight(
-                    code: '''
-MaterialApp.router(
-  routeInformationParser: BeamerParser(),
-  routerDelegate: routerDelegate,
-);''',
-                    textStyle: TextStyle(fontSize: 24),
-                  ),
-                  SizedBox(height: 16),
-                  FlutterDeckCodeHighlight(
-                    code: '''Beamer.of(context).beamToNamed('/route');''',
+final player = AudioPlayer();
+final duration = await player.setUrl('https://foo.com/bar.mp3');
+await player.play();
+await player.pause();
+await player.seek(Duration(second: 10));
+await player.setSpeed(2.0);
+await player.setVolume(0.5);
+await player.stop();''',
                     textStyle: TextStyle(fontSize: 24),
                   ),
                 ],
