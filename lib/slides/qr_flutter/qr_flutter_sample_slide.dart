51fe203a96e3f3726160c2cc9a0af3f86b472cd7
Goddchen <goddchen@gmail.com>
2023-09-10 17:32:23 +0200

    feat: add qr_flutter

---
+++ lib/slides/qr_flutter/qr_flutter_sample_slide.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_deck/flutter_deck.dart';
+import 'package:flutter_packages_a_z/gen/assets.gen.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class QrFlutterSampleSlide extends FlutterDeckSplitSlide {
+  const QrFlutterSampleSlide({super.key})
+      : super(
+          configuration: const FlutterDeckSlideConfiguration(
+            header: FlutterDeckHeaderConfiguration(title: 'qr_flutter'),
+            route: '/qr-flutter-sample',
+          ),
+        );
+
+  @override
+  Widget left(BuildContext context) => const FlutterDeckCodeHighlight(
+        code: '''
+QrImageView(
+  backgroundColor: Theme.of(context).colorScheme.primary,
+  data: 'https://pub.dev/packages/qr_flutter',
+  dataModuleStyle: QrDataModuleStyle(
+    color: Theme.of(context).colorScheme.onPrimary,
+    dataModuleShape: QrDataModuleShape.square,
+  ),
+  eyeStyle: QrEyeStyle(
+    color: Theme.of(context).colorScheme.onPrimary,
+    eyeShape: QrEyeShape.square,
+  ),
+);''',
+        textStyle: TextStyle(fontSize: 20),
+      );
+
+  @override
+  Widget right(BuildContext context) => QrImageView(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        data: 'https://pub.dev/packages/qr_flutter',
+        dataModuleStyle: QrDataModuleStyle(
+          color: Theme.of(context).colorScheme.onPrimary,
+          dataModuleShape: QrDataModuleShape.square,
+        ),
+        eyeStyle: QrEyeStyle(
+          color: Theme.of(context).colorScheme.onPrimary,
+          eyeShape: QrEyeShape.square,
+        ),
+      );
+}
