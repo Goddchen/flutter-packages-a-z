85a6ef8561b6ff923f7822a67d3a1d3c92cc95ec
Goddchen <goddchen@gmail.com>
2023-09-10 17:32:51 +0200

    feat: remove unused import

--- lib/slides/qr_flutter/qr_flutter_sample_slide.dart
+++ lib/slides/qr_flutter/qr_flutter_sample_slide.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_deck/flutter_deck.dart';
-import 'package:flutter_packages_a_z/gen/assets.gen.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 class QrFlutterSampleSlide extends FlutterDeckSplitSlide {
