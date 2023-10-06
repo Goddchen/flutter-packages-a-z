f7b351ed32f9a2fe491c36700fe2d2265d9a266c
Goddchen <goddchen@gmail.com>
2023-10-06 20:43:13 +0200

    refactor: make OSM widget a stateful widget

--- lib/slides/flutter_osm_plugin/flutter_osm_plugin_sample_slide.dart
+++ lib/slides/flutter_osm_plugin/flutter_osm_plugin_sample_slide.dart
@@ -7,48 +7,79 @@ class FlutterOsmPluginSampleSlide extends SampleSlide {
       : super(
           route: '/flutter-osm-plugin-sample',
           sampleCode: '''
-final GeoPoint home = GeoPoint(
-  latitude: 47.672982,
-  longitude: 9.165711,
-);
-return OSMFlutter(
-  controller: MapController.withPosition(initPosition: home),
-  osmOption: OSMOption(
-    staticPoints: <StaticPositionGeoPoint>[
-      StaticPositionGeoPoint(
-        'home',
-        const MarkerIcon(icon: Icon(Icons.home)),
-        <GeoPoint>[
-          home,
-        ],
-      ),
-    ],
-    zoomOption: const ZoomOption(initZoom: 12),
-  ),
-);''',
-          sampleWidget: Builder(
-            builder: (_) {
-              final GeoPoint home = GeoPoint(
-                latitude: 47.672982,
-                longitude: 9.165711,
-              );
-              return OSMFlutter(
-                controller: MapController.withPosition(initPosition: home),
-                osmOption: OSMOption(
-                  staticPoints: <StaticPositionGeoPoint>[
-                    StaticPositionGeoPoint(
-                      'home',
-                      const MarkerIcon(icon: Icon(Icons.home)),
-                      <GeoPoint>[
-                        home,
-                      ],
-                    ),
-                  ],
-                  zoomOption: const ZoomOption(initZoom: 12),
-                ),
-              );
-            },
-          ),
+class _MapWidgetState extends State<_MapWidget> {
+  static final GeoPoint _home = GeoPoint(
+    latitude: 47.672982,
+    longitude: 9.165711,
+  );
+
+  final MapController _mapController =
+      MapController.withPosition(initPosition: _home);
+
+  @override
+  void dispose() {
+    _mapController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => OSMFlutter(
+        controller: _mapController,
+        osmOption: OSMOption(
+          staticPoints: <StaticPositionGeoPoint>[
+            StaticPositionGeoPoint(
+              'home',
+              const MarkerIcon(icon: Icon(Icons.home)),
+              <GeoPoint>[
+                _home,
+              ],
+            ),
+          ],
+          zoomOption: const ZoomOption(initZoom: 12),
+        ),
+      );
+}''',
+          sampleWidget: const _MapWidget(),
           title: 'flutter_osm_plugin',
         );
+}
+
+class _MapWidget extends StatefulWidget {
+  const _MapWidget();
+
+  @override
+  State<_MapWidget> createState() => _MapWidgetState();
+}
+
+class _MapWidgetState extends State<_MapWidget> {
+  static final GeoPoint _home = GeoPoint(
+    latitude: 47.672982,
+    longitude: 9.165711,
+  );
+
+  final MapController _mapController =
+      MapController.withPosition(initPosition: _home);
+
+  @override
+  void dispose() {
+    _mapController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => OSMFlutter(
+        controller: _mapController,
+        osmOption: OSMOption(
+          staticPoints: <StaticPositionGeoPoint>[
+            StaticPositionGeoPoint(
+              'home',
+              const MarkerIcon(icon: Icon(Icons.home)),
+              <GeoPoint>[
+                _home,
+              ],
+            ),
+          ],
+          zoomOption: const ZoomOption(initZoom: 12),
+        ),
+      );
 }
